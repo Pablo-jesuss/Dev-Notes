b512cafbb39ef20dfe54c1812ce6e59dc1fc2450
Pablo-jesuss <pablo.programador26@gmail.com>
2024-03-07 11:26:10 -0300

    javascript

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,4 @@
-
+// Elementos
+const notesContainer = document.querySelector("#notes-container");
+const noteInput = document.querySelector("#note-content");
+const addNoteBtn = document.querySelector(".add-note");
