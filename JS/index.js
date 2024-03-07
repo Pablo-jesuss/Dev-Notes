7100b9a225a81f287522da852942849cbb7aaf52
Pablo-jesuss <pablo.programador26@gmail.com>
2024-03-07 11:37:55 -0300

    generateId

--- JS/index.js
+++ JS/index.js
@@ -2,3 +2,20 @@
 const notesContainer = document.querySelector("#notes-container");
 const noteInput = document.querySelector("#note-content");
 const addNoteBtn = document.querySelector(".add-note");
+
+// Funções
+function addNote() {
+  const noteObject = {
+    id: generateId(),
+    content: noteInput.value,
+    fixed: false,
+  };
+
+  console.log(noteObject);
+}
+
+function generateId() {
+  return Math.floor(Math.random() * 5000);
+}
+// Eventos
+addNoteBtn.addEventListener("click", () => addNote());
