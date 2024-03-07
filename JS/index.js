2532bc100a770098ff1ac27361cf11d42c36fb2e
Pablo-jesuss <pablo.programador26@gmail.com>
2024-03-07 11:47:46 -0300

    createNote

--- JS/index.js
+++ JS/index.js
@@ -11,11 +11,30 @@ function addNote() {
     fixed: false,
   };
 
-  console.log(noteObject);
+  const noteElement = createNote(noteObject.id, noteObject.content);
+
+  notesContainer.appendChild(noteElement);
 }
 
 function generateId() {
   return Math.floor(Math.random() * 5000);
 }
+
+function createNote(id, content, fixed) {
+  const element = document.createElement("div");
+
+  element.classList.add("note");
+
+  const textarea = document.createElement("textarea");
+
+  textarea.value = content;
+
+  textarea.placeholder = "Adicione algum texto...";
+
+  element.appendChild(textarea);
+
+  return element;
+}
+
 // Eventos
 addNoteBtn.addEventListener("click", () => addNote());
